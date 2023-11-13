5026194a84a2ec5c2c102e0d2a19b6a7496ae22d
ericareven <erica.reven@gmail.com>
2023-11-13 15:07:47 -0500

    generate rows and columns

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
+# MODULES
+import random
+
 MAX_LINES = 3 # constant values are in all caps
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = { # dictionary
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items(): # gets key and value
+        for _ in range(symbol_count): # _ anonymous value
+            all_symbols.append(symbol)
+    
+    columns = [] # columns list
+    for _ in range(cols):
+        column = [] 
+        current_symbols = all_symbols[:] # : slice operator (copy of all_symbols list)
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column) # add column to column list
+        
+    return columns
 
 def deposit(): # function
     while True:
@@ -48,7 +80,7 @@ def main():
     while True:
         bet = get_bet()
         total_bet = bet * lines
-        
+
         if total_bet > balance:
             print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
         else:
